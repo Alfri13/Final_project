7c62e455fca30ca65801697c1481aadc7b2138c1
Alfri13 <alfri13@gmail.com>
2021-04-16 14:28:10 -0500

    Java Script addition
    I have added a java script jquery function. I'm still woking on it to function as our prototype

--- java/index.js
+++ java/index.js
@@ -1,3 +1,22 @@
-$(function(){
-    
-}
+var helloWorld = "test";
+
+console.log(helloWorld);
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  $('#date-picker').mobiscroll().datepicker({
+    controls: ['date'],
+    touchUi: true
+});
+
+// time picker
+$('#time-picker').mobiscroll().datepicker({
+    controls: ['time'],
+    touchUi: true
+});
